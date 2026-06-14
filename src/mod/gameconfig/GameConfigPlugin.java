80a6294a9efc26143af21f0046f398f642fc2398
Darkborderman <reastw1234@gmail.com>
2026-06-14 12:39:13 +0800

    Update

---
+++ src/mod/gameconfig/GameConfigPlugin.java
@@ -0,0 +1,26 @@
+package mod.gameconfig;
+
+import com.fs.starfarer.api.BaseModPlugin;
+import com.fs.starfarer.api.Global;
+import com.fs.starfarer.api.combat.ShipHullSpecAPI;
+import com.fs.starfarer.api.combat.ShipHullSpecAPI.ShipTypeHints;
+import com.fs.starfarer.api.campaign.FactionSpecAPI;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+
+public class GameConfigPlugin extends BaseModPlugin {
+
+    public void onGameLoad(boolean newGame) {
+        for (FactionSpecAPI faction : Global.getSettings().getAllFactionSpecs()) {
+            try {
+                JSONObject custom = faction.getCustom();
+                if (custom == null) continue;
+                custom.put("caresAboutAtrocities", true);
+                faction.setCustom(custom);
+            } catch (JSONException e) {
+                Global.getLogger(GameConfigPlugin.class).warn("Failed to set caresAboutAtrocities for faction: " + faction.getId(), e);
+            }
+        }
+    }
+}
